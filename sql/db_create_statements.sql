029075e79de2e11e39b1b1352d327d04d63ca428
Marisa Ries <97467505+MarisaRies@users.noreply.github.com>
2022-01-25 22:12:15 +0100

    Update db_create_statements.sql

--- sql/db_create_statements.sql
+++ sql/db_create_statements.sql
@@ -18,30 +18,34 @@ CREATE TABLE tb_patients
      id                   NVARCHAR(256), 
      start                TIMESTAMP DEFAULT NULL, 
      stop                 TIMESTAMP DEFAULT NULL, 
-     patient_id           NVARCHAR(256), 
+     patient              NVARCHAR(256), 
      code                 NVARCHAR(256), 
      description          NVARCHAR(256) DEFAULT NULL,
      PRIMARY KEY (id, code), 
-     FOREIGN KEY (patient_id) REFERENCES tb_patients(id)
+     FOREIGN KEY (patient) REFERENCES tb_patients(id)
   );
 
 CREATE TABLE tb_conditions 
   ( 
+     encounter      NVARCHAR(256),  
      start          DATE, 
      stop           DATE, 
-     patient_id     NVARCHAR(256), 
+     patient        NVARCHAR(256), 
      code           NVARCHAR(256), 
      description    NVARCHAR(256) DEFAULT NULL, 
-     PRIMARY KEY (start, stop, patient_id, code), 
-     FOREIGN KEY (patient_id) REFERENCES tb_patients(id) 
+     PRIMARY KEY (encounter, start, stop, patient, code), 
+     FOREIGN KEY (patient) REFERENCES tb_patients(id),
+     FOREIGN KEY (encounter) REFERENCES tb_encounters (id) 
   );
   
   CREATE TABLE tb_immunizations 
   ( 
+     encounter      NVARCHAR(256),   
      date           TIMESTAMP, 
-     patient_id     NVARCHAR(256), 
+     patient        NVARCHAR(256), 
      code           NVARCHAR(256), 
      description    NVARCHAR(256) DEFAULT NULL, 
-     PRIMARY KEY (date, patient_id, code), 
-     FOREIGN KEY (patient_id) REFERENCES tb_patients(id)
+     PRIMARY KEY (encounter, date, patient, code), 
+     FOREIGN KEY (patient) REFERENCES tb_patients(id),
+     FOREIGN KEY (encounter) REFERENCES tb_encounters (id) 
   );
