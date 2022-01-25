c8045eb54690cb38f0831843e4951fc6e38077d1
Marisa Ries <97467505+MarisaRies@users.noreply.github.com>
2022-01-25 02:46:23 +0100

    Update db_create_statements.sql

--- sql/db_create_statements.sql
+++ sql/db_create_statements.sql
@@ -8,8 +8,7 @@ CREATE TABLE tb_patients
      deathdate           DATE DEFAULT NULL, 
      first               NVARCHAR(256) DEFAULT NULL, 
      last                NVARCHAR(256) DEFAULT NULL, 
-     dataset_origin      NVARCHAR(256), 
-     PRIMARY KEY (id, dataset_origin) 
+     PRIMARY KEY (id) 
   );
 
 CREATE TABLE tb_conditions 
@@ -19,7 +18,6 @@ CREATE TABLE tb_conditions
      patient_id     NVARCHAR(256), 
      code           NVARCHAR(256), 
      description    NVARCHAR(256) DEFAULT NULL, 
-     dataset_origin NVARCHAR(256), 
-     PRIMARY KEY (start, stop, patient_id, code, dataset_origin), 
-     FOREIGN KEY (patient_id) REFERENCES tb_patients(id, dataset_origin) 
+     PRIMARY KEY (start, stop, patient_id, code), 
+     FOREIGN KEY (patient_id) REFERENCES tb_patients(id) 
   );
