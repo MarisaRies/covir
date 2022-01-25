89ad2b29caf49082f3a5eb4ac781727b317a73c6
Marisa Ries <97467505+MarisaRies@users.noreply.github.com>
2022-01-25 02:36:25 +0100

    Create db_create_statements.sql

---
+++ sql/db_create_statements.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS tb_conditions; 
+DROP TABLE IF EXISTS tb_patients; 
+
+CREATE TABLE tb_patients 
+  ( 
+     id                  NVARCHAR(256), 
+     birthdate           DATE DEFAULT NULL, 
+     deathdate           DATE DEFAULT NULL, 
+     first               NVARCHAR(256) DEFAULT NULL, 
+     last                NVARCHAR(256) DEFAULT NULL, 
+     dataset_origin      NVARCHAR(256), 
+     PRIMARY KEY (id, dataset_origin) 
+  );
+
+CREATE TABLE tb_conditions 
+  ( 
+     start          DATE, 
+     stop           DATE, 
+     patient_id     NVARCHAR(256), 
+     code           NVARCHAR(256), 
+     description    NVARCHAR(256) DEFAULT NULL, 
+     dataset_origin NVARCHAR(256), 
+     PRIMARY KEY (start, stop, patient_id, code, dataset_origin), 
+     FOREIGN KEY (patient_id) REFERENCES tb_patients(id, dataset_origin), 
+  );
