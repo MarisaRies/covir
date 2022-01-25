d54a68927fad54c080a83a98e0d1b5280b6c766a
Marisa Ries <97467505+MarisaRies@users.noreply.github.com>
2022-01-25 19:44:46 +0100

    Update db_create_statements.sql

--- sql/db_create_statements.sql
+++ sql/db_create_statements.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS tb_immunizations; 
-DROP TABLE IF EXISTS tb_conditions; 
+DROP TABLE IF EXISTS tb_conditions;
+DROP TABLE IF EXISTS tb_encounters; 
 DROP TABLE IF EXISTS tb_patients; 
 
 CREATE TABLE tb_patients 
@@ -10,6 +11,18 @@ CREATE TABLE tb_patients
      first               NVARCHAR(256) DEFAULT NULL, 
      last                NVARCHAR(256) DEFAULT NULL, 
      PRIMARY KEY (id) 
+  );
+  
+  CREATE TABLE tb_encounters 
+  ( 
+     id                   NVARCHAR(256), 
+     start                TIMESTAMP DEFAULT NULL, 
+     stop                 TIMESTAMP DEFAULT NULL, 
+     patient_id           NVARCHAR(256), 
+     code                 NVARCHAR(256), 
+     description          NVARCHAR(256) DEFAULT NULL,
+     PRIMARY KEY (id, code), 
+     FOREIGN KEY (patient_id) REFERENCES tb_patients(id)
   );
 
 CREATE TABLE tb_conditions 
