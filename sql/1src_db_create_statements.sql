6a0f391efa11946986e83f4f4766fa6e1eb32346
Marisa Ries <97467505+MarisaRies@users.noreply.github.com>
2022-01-27 18:09:29 +0100

    Update and rename src_db_create_statements.sql to 1src_db_create_statements.sql

--- sql/1src_db_create_statements.sql
+++ sql/1src_db_create_statements.sql
@@ -7,8 +7,7 @@ CREATE TABLE tb_patients
   ( 
      id                  NVARCHAR(256), 
      birthdate           DATE DEFAULT NULL, 
-     deathdate           DATE DEFAULT NULL, 
-     age                 INT,
+     deathdate           DATE DEFAULT NULL,
      first               NVARCHAR(256) DEFAULT NULL, 
      last                NVARCHAR(256) DEFAULT NULL, 
      PRIMARY KEY (id) 
